4cb8d5dfdec0355eac5d309deaac6d866b2eaf53
Scientist DATA <marius.akre@edu.dsti.institute>
2025-03-06 16:09:17 +0100

    Ajouter le deuxième cas d'usage avec entrainement et prédiction.

--- streamlit_app/pages/2_📥_Dépot_et_Validation_des_Données.py
+++ streamlit_app/pages/2_📥_Dépot_et_Validation_des_Données.py
@@ -12,8 +12,8 @@ preprocessing_dir = "streamlit_app/static/dossier_donnees/donnees_preprocessees/
 def show():
     st.title("Dépot et Validation des Données")
 
-    ###if 'input_data'  in st.session_state:
-        ###st.write(st.session_state.input_data)
+    ###if 'prediction_data'  in st.session_state:
+        ###st.write(st.session_state.prediction_data)
 
     check_dependencies("Dépot et Validation des Données")
 
@@ -24,30 +24,31 @@ def show():
 
     if uploaded_file is not None:
         try:
-            input_data = pd.read_csv(uploaded_file, header=None)
-            st.write("Affichage des données d'entrée pour la prédiction :", input_data)
+            prediction_data = pd.read_csv(uploaded_file, header=None)
+            st.write("Affichage des données d'entrée pour la prédiction :", prediction_data)
         except Exception as e:
             st.error(f"Erreur lors de la lecture du fichier : {e}")
-            input_data = None
+            prediction_data = None
     else:
         # Jeu de données de test par défaut afin de pouvoir effectuer des tests
-        input_data = pd.read_csv(preprocessing_dir + "donnees_par_defaut/x_valid_s65_o60_p5-1.csv", header=None)
-        st.write(f"Format des données d'entrée par défaut pour la prédiction - Nombre de lignes: {input_data.shape[0]:,}, Nombre de colonnes: {input_data.shape[1]}")
-        st.write("Apperçu des données d'entrée par défaut pour la prédiction:", input_data)
+        prediction_data = pd.read_csv(preprocessing_dir + "donnees_par_defaut/x_valid_s65_o60_p5-1.csv", header=None)
+        st.write(f"Format des données d'entrée par défaut pour la prédiction - Nombre de lignes: {prediction_data.shape[0]:,}, Nombre de colonnes: {prediction_data.shape[1]}")
+        st.write("Apperçu des données d'entrée par défaut pour la prédiction:", prediction_data)
 
+    # Bouton pour valider les données
     if st.button("Validation des données"):
-        if input_data is not None:
-            if input_data.isna().values.any():
+        if prediction_data is not None:
+            if prediction_data.isna().values.any():
                 st.error("Erreur: Les données contiennent des valeurs manquantes.")
-            elif (input_data < 0).values.any():
+            elif (prediction_data < 0).values.any():
                 st.error("Erreur: Les données contiennent des valeurs négatives.")
             else:
-                # Convert input_data to a DataFrame with a 'value' column
-                input_data_df = pd.DataFrame({'value': input_data.values.flatten()})
-                input_data_df.index = range(1, len(input_data_df) + 1)
+                # Convert prediction_data to a DataFrame with a 'value' column
+                prediction_data_df = pd.DataFrame({'value': prediction_data.values.flatten()})
+                prediction_data_df.index = range(1, len(prediction_data_df) + 1)
 
                 # Store properly formatted data in session state
-                st.session_state.input_data = input_data_df
+                st.session_state.prediction_data = prediction_data_df
 
                 st.success("Les données sont valides.")
                 st.session_state.prediction_effectuee = False
