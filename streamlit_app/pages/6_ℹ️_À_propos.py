eeb1c8d1de196d43a55e88beb9c0b1fe97d95d20
Scientist DATA <marius.akre@edu.dsti.institute>
2025-04-05 09:58:32 +0200

    Déplacer le champ de saisie de la date de la première observation vers la page de dépôt des données observées. MAJ les infos générales de l'App

--- streamlit_app/pages/6_ℹ️_À_propos.py
+++ streamlit_app/pages/6_ℹ️_À_propos.py
@@ -6,7 +6,7 @@ display_menu()
 def show():
     st.title("À propos")
     st.write("""
-    Cette plateforme a été développée dans le cadre d'un hackathon pour prédire les valeurs des 5 prochaines secondes à partir de 60 secondes de données.
+    Cette plateforme a été développée dans le cadre d'un hackathon pour prédire les valeurs des 1, 5, 30, 60 ou 300 prochaines secondes à partir de 12, 60, 300, 400 ou 500 secondes de données.
     """)
 
 if __name__ == "__main__":
